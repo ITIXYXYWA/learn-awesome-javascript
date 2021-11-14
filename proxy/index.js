8ed41aacb629016d277587b92bd4715a9bc844df
Dmitry Vlasenko <67154333+DmitryqV@users.noreply.github.com>
2021-11-14 13:57:45 +0500

    learn proxy

---
+++ proxy/index.js
@@ -0,0 +1,28 @@
+let target = { _secure: false, _admin: 'Admin', _password: '12312412412', admin: true };
+target = new Proxy(target, {
+
+  get(target, prop) {
+    if (prop in target) {
+      return prop !== '_secure' && target[prop];
+    }
+    return undefined;
+  },
+
+  set(target, prop, val) {
+    return prop === '_secure' ? target[prop] = val : new Error('dont change secure');;
+  },
+
+  ownKeys(target) {
+    return Object.keys(target).filter(key => !key.startsWith('_'));
+  },
+
+  deleteProperty(target, prop) {
+    if (prop.startsWith('_')) {
+      return new Error("Access denied");
+    };
+    return delete target[prop];
+  },
+
+});
+
+console.log(target);
